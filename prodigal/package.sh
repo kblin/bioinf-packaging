dbdf10b74a60a2dbedad39c0aafd568a373f8c8d
Kai Blin <kblin@biosustain.dtu.dk>
2016-12-31 10:21:31 +0100

    prodigal: update to version 2.6.3
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- prodigal/package.sh
+++ prodigal/package.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-VERSION="2.6.2"
+VERSION="2.6.3"
 
 wget https://github.com/hyattpd/Prodigal/releases/download/v${VERSION}/prodigal.linux -O prodigal
+chmod a+x prodigal
 
 fpm -s dir -t deb -n prodigal -v ${VERSION} --iteration 1 -m "kblin@biosustain.dtu.dk" ./prodigal=/usr/bin/prodigal
