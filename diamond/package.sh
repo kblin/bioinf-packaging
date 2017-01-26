017dd957a2f9059ae72fcc83de825b6ce75a35a3
Kai Blin <kblin@biosustain.dtu.dk>
2017-01-26 17:34:38 +0100

    diamond: Always package the official diamond build
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- diamond/package.sh
+++ diamond/package.sh
@@ -2,4 +2,9 @@
 
 VERSION="0.8.31"
 
-fpm -s dir -t deb -n diamond -v ${VERSION} --iteration 1 -m "kblin@biosustain.dtu.dk" ./diamond=/usr/bin/diamond
+wget https://github.com/bbuchfink/diamond/releases/download/v${VERSION}/diamond-linux64.tar.gz
+tar xf diamond-linux64.tar.gz diamond
+
+fpm -s dir -t deb -n diamond -v ${VERSION} --iteration 2 -m "kblin@biosustain.dtu.dk" ./diamond=/usr/bin/diamond
+
+rm diamond-linux64.tar.gz diamond
